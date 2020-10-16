c5afde37550ade511e3f9e578c4702e4ed14d281
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-10-16 04:59:20 +0000

    Create vectors.cpp

---
+++ vectors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> num;
+    for (int i=0; i < 10; i++)
+        num.push_back(i);
+    for (int i=0; i < 10; i++)
+        cout << num[i] << " ";
+    return 0;
+}
